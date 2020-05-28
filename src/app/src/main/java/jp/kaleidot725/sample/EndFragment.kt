70e302bc211380332299e50e2881061296da99f3
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-28 22:11:13 +0900

    Add Project

---
+++ src/app/src/main/java/jp/kaleidot725/sample/EndFragment.kt
@@ -0,0 +1,9 @@
+package jp.kaleidot725.sample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EndFragment : Fragment(R.layout.fragment_end)
